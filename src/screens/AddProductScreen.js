635ae4f9200e6a056bfd0ab6b15b229386564e88
Eirini Lygerou <irini.lyg@gmail.com>
2021-11-14 18:34:49 +0200

    Add AddProductStyle

--- src/screens/AddProductScreen.js
+++ src/screens/AddProductScreen.js
@@ -1,10 +1,11 @@
 import React from "react";
 
-import { StyleSheet, View, ScrollView, Text, TextInput, TouchableOpacity, Alert } from 'react-native';
+import {View, ScrollView, Text, TextInput, TouchableOpacity, Alert } from 'react-native';
 import PickerSelect from 'react-native-picker-select';
 import { CheckedIcon, UncheckedIcon } from '../images/svg-icons';
 import { connect } from "react-redux";
 import { add, userID } from '../utils/utils'
+import {AddProductStyle} from "../AppStyles";
 
 
 const mapStateToProps = state => ({
@@ -59,12 +60,12 @@ const AddProductScreen = function ({ navigation }) {
   };
 
   return (
-    <ScrollView style={styles.outerView}>
-      <View style={styles.splitView}>
-        <View style={styles.typeArea}>
-          <Text style={styles.label}>Type</Text>
+    <ScrollView style={AddProductStyle.outerView}>
+      <View style={AddProductStyle.splitView}>
+        <View style={AddProductStyle.typeArea}>
+          <Text style={AddProductStyle.label}>Type</Text>
           <PickerSelect
-            style={{ inputIOS: styles.selector }}
+            style={{ inputIOS: AddProductStyle.selector }}
             value={item.type}
             onValueChange={(t) => setItem({ ...item, type: t })}
             items={[
@@ -72,14 +73,14 @@ const AddProductScreen = function ({ navigation }) {
             ]}
           />
         </View>
-        <View style={styles.quantityArea}>
-          <Text style={styles.label}>Quantity</Text>
+        <View style={AddProductStyle.quantityArea}>
+          <Text style={AddProductStyle.label}>Quantity</Text>
           <TextInput
-            style={styles.textInput}
+            style={AddProductStyle.textInput}
             value={item.quantity}
             onChangeText={(t) => setItem({ ...item, quantity: t})}
             onSubmitEditing={sendItem}
-            returnKeyType='add'
+            returnKeyType='send'
             enablesReturnKeyAutomatically={true}
             placeholder='e.g., 10'
             keyboardType='numeric'
@@ -87,39 +88,39 @@ const AddProductScreen = function ({ navigation }) {
         </View>
       </View>
   
-      <Text style={styles.label}>Name</Text>
+      <Text style={AddProductStyle.label}>Name</Text>
       <TextInput
-        style={styles.textInput}
+        style={AddProductStyle.textInput}
         value={item.name}
         onChangeText={(t) => setItem({ ...item, name: t})}
         onSubmitEditing={sendItem}
-        returnKeyType='add'
+        returnKeyType='send'
         enablesReturnKeyAutomatically={true}
         placeholder='e.g., 10 kWh'
         blurOnSubmit={false}
       />
-      <Text style={styles.label}>Contact</Text>
+      <Text style={AddProductStyle.label}>Contact</Text>
       <TextInput
-        style={styles.textInput}
+        style={AddProductStyle.textInput}
         value={item.contact}
         onChangeText={(t) => setItem({ ...item, contact: t})}
         onSubmitEditing={sendItem}
-        returnKeyType='add'
+        returnKeyType='send'
         enablesReturnKeyAutomatically={true}
         placeholder='user@domain.com'
       />
-      <Text style={styles.label}>Description</Text>
+      <Text style={AddProductStyle.label}>Description</Text>
       <TextInput
-        style={styles.textInput}
+        style={AddProductStyle.textInput}
         value={item.description}
         onChangeText={(t) => setItem({ ...item, description: t})}
         onSubmitEditing={sendItem}
-        returnKeyType='add'
+        returnKeyType='send'
         enablesReturnKeyAutomatically={true}
         placeholder='e.g., 10 kWh from solar panels'
       />
-      <Text style={styles.label}>Location</Text>
-      <View style={styles.checkboxContainer}>
+      <Text style={AddProductStyle.label}>Location</Text>
+      <View style={AddProductStyle.checkboxContainer}>
         <TouchableOpacity onPress={toggleUseLocation}>
           {
             (useLocation)
@@ -129,14 +130,14 @@ const AddProductScreen = function ({ navigation }) {
               <UncheckedIcon height='18' width='18'/>
           }
         </TouchableOpacity>
-        <Text style={styles.checkboxLabel}> Use my current location </Text>
+        <Text style={AddProductStyle.checkboxLabel}> Use my current location </Text>
       </View>
       <TextInput
-        style={useLocation ? styles.textInputDisabled : styles.textInput}
+        style={useLocation ? AddProductStyle.textInputDisabled : AddProductStyle.textInput}
         value={item.location}
         onChangeText={(t) => setItem({ ...item, location: t})}
         onSubmitEditing={sendItem}
-        returnKeyType='add'
+        returnKeyType='send'
         enablesReturnKeyAutomatically={true}
         placeholder='street address, city, state'
         editable={!useLocation}
@@ -147,81 +148,13 @@ const AddProductScreen = function ({ navigation }) {
         item.name.trim() !== '' &&
         item.contact.trim() !== '' &&
         <TouchableOpacity onPress={sendItem}>
-          <Text style={styles.button}>Add</Text>
+          <Text style={AddProductStyle.button}>Add</Text>
         </TouchableOpacity>
       }
     </ScrollView>
   );
 
 };
-
-const styles = StyleSheet.create({
-  outerView: {
-    flex: 1,
-    padding: 22,
-    backgroundColor: '#FFF'
-  },
-  splitView: {
-    flexDirection: 'row',
-    justifyContent: 'space-between'
-  },
-  typeArea: {
-    width: '40%'
-  },
-  label: {
-    fontFamily: 'IBMPlexSans-Medium',
-    color: '#000',
-    fontSize: 14,
-    paddingBottom: 5
-  },
-  selector: {
-    fontFamily: 'IBMPlexSans-Medium',
-    borderColor: '#D0E2FF',
-    borderWidth: 2,
-    padding: 16,
-    marginBottom: 25
-  },
-  quantityArea: {
-    width: '40%'
-  },
-  textInput: {
-    fontFamily: 'IBMPlexSans-Medium',
-    flex: 1,
-    borderColor: '#D0E2FF',
-    borderWidth: 2,
-    padding: 14,
-    elevation: 2,
-    marginBottom: 25
-  },
-  checkboxContainer: {
-    flexDirection: 'row',
-    alignItems: 'flex-start',
-    marginBottom: 10
-  },
-  checkboxLabel: {
-    fontFamily: 'IBMPlexSans-Light',
-    fontSize: 13
-  },
-  textInputDisabled: {
-    fontFamily: 'IBMPlexSans-Medium',
-    backgroundColor: '#f4f4f4',
-    color: '#999',
-    flex: 1,
-    padding: 16,
-    elevation: 2,
-    marginBottom: 25
-  },
-  button: {
-    backgroundColor: '#1062FE',
-    color: '#FFFFFF',
-    fontFamily: 'IBMPlexSans-Medium',
-    fontSize: 16,
-    overflow: 'hidden',
-    padding: 12,
-    textAlign:'center',
-    marginTop: 15
-  }
-});
 
 
 export default connect(mapStateToProps)(AddProductScreen);
