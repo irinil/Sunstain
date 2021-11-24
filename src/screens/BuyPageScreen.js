626e044f65fc0b41efdf8e98729d0ec11695cf66
Eirini Lygerou <irini.lyg@gmail.com>
2021-11-24 22:58:47 +0200

    Update BuyPageScreen

--- src/screens/BuyPageScreen.js
+++ src/screens/BuyPageScreen.js
@@ -9,14 +9,16 @@ import {
 import {BuyProductStyle} from "../AppStyles";
 import {AddProductStyle} from "../AppStyles";
 
-const BuyPageScreen =(props) => {
-
 const sendItem = () => {
-    Alert.alert('Thank you!', 'Your Service provider has accepted your request and the provided amount will be discounted from your next electricity bill.', [{text: 'OK'}]);
-  };
+  Alert.alert('Thank you!', 'Your Service provider has accepted your request and the provided amount will be discounted from your next electricity bill.', [{text: 'OK'}]);
+};
+
+const BuyPageScreen =(props) => {
+const [items, setItems] = React.useState([]);
+
         return (
             <ScrollView style={AddProductStyle.outerView}>
-                    <Text style={BuyProductStyle.title}>Name: {props.name}</Text>
+                    <Text style={BuyProductStyle.title}>Name: {props.name} </Text>
                     <Text style={BuyProductStyle.price}> Quantity : {props.quantity} </Text>
                     <Text style={BuyProductStyle.price}> Contact : {props.contact} </Text>
                     <Text style={BuyProductStyle.price}> Price : {props.price} </Text>
